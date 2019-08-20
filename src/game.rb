ad51972a1d6caa4854b597d12322f818e50c0cb7
shivusolanki <41575087+shivusolanki@users.noreply.github.com>
2019-08-20 19:23:06 +0530

    Update game.rb

--- src/game.rb
+++ src/game.rb
@@ -18,56 +18,20 @@ class Game
     puts
     puts "***Let the game begin***"
     puts
-
+	  
     play
+    won_or_tie?(board)
+    puts
+    print "Wanna play again (y/n) ? : "
+    restart = gets.chomp
 
-<<<<<<< HEAD
-		won_or_tie?(board)
-		puts
-		print "Wanna play again (y/n) ? : "
-		restart = gets.chomp
-
-		if (restart == "y" || restart == "Y")
-			@board = Board.new
-			Game.start
-		else
-			puts
-			puts "bye bye!!"
-			exit
-		end
-	end
-
-	def play
-		board.board_state
-		until board.game_over?
-			player1.choose_spot(board, 'X')
-			board.board_state
-			player2.choose_spot(board, 'O') if !board.game_over?
-			board.board_state
-		end
-	end
-
-	private
-
-	def won_or_tie?(board)
-		if board.won?
-	  	winner = board.player_win
-		  puts "player with token [ #{winner} ] is winner!!"
-			puts "\nGAME OVER"
-		else 
-			puts "Game is tie !!"
-			puts "\nGAME OVER"
-		end
-	end
-end
-=======
-    if board.won?
-      winner = board.player_win
-      puts "player with token [ #{winner} ] is winner!!"
-      puts "\nGame Over"
-    else 
-      puts "Game is tie !!"
-      puts "\nGame Over"
+    if (restart == "y" || restart == "Y")
+      @board = Board.new
+      Game.start
+    else
+      puts
+      puts "bye bye!!"
+      exit
     end
   end
 
@@ -75,9 +39,22 @@ end
     board.board_state
     until board.game_over?
     player1.choose_spot(board, 'X')
+    board.board_state
     player2.choose_spot(board, 'O') if !board.game_over?
     board.board_state
     end
   end
+	
+  private
+
+  def won_or_tie?(board)
+    if board.won?
+      winner = board.player_win
+      puts "player with token [ #{winner} ] is winner!!"
+      puts "\nGAME OVER"
+    else 
+      puts "Game is tie !!"
+      puts "\nGAME OVER"
+    end
+  end
 end
->>>>>>> f7e834f93e0526c877d98feb7b07c845c16383a3
